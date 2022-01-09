8bb44f54d1bc3cadcec07bb8040f58f61d06b4fe
Sukalyan <sukalyandash1920@gmail.com>
2022-01-09 22:29:41 +0530

    Employee Wage Objects Programs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWageObjectsProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1 uc = new UC1();
+            //uc.GetAttendance();
+            //UC2 UC2 = new UC2();
+            //UC2.EmpDailyWage();
+            //UC3 UC3 = new UC3();
+            //UC3.PartTimeEmpWage();
+            //UC4 UC4 = new UC4();
+            //UC4.EmpWageUsingSwitch();
+            //UC5 UC5 = new UC5();
+            //UC5.CalculateWages();
+              UC6 UC6 = new UC6();
+              UC6.CalculateWageTill100HrsOr20IsReached();
+        }
+    }
+}
---
+++ UC1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageObjectsProblem
+{
+    class UC1
+    {
+            public void GetAttendance()
+            {
+                int IS_FULL_TIME = 1;
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                if (empCheck == IS_FULL_TIME)
+                    Console.WriteLine("Employee Present");
+                else
+                    Console.WriteLine("Employee Absent");
+            }
+        
+    }
+    
+}
+
+
